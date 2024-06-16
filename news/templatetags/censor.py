eba6592a5ad28117a1c224c7dcd3dd7acea93cf5
AleKolar <gefest-173@rambler.ru>
2024-06-16 17:14:28 +0300

    commit8

--- news/templatetags/censor.py
+++ news/templatetags/censor.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.filter(name='censor')
 def censor(value):
-    banned_words = ['badword1', 'badword2']
+    banned_words = ['badword', 'badword1', 'badword2']
     for word in banned_words:
         if isinstance(word, str):
             value = value.replace(word[1:], '*' * len(word[1:]))
