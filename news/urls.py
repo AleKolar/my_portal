a00265aa57681096d91dad9e01943567a5ae5633
AleKolar <gefest-173@rambler.ru>
2024-06-19 06:18:07 +0300

    commit12

--- news/urls.py
+++ news/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
 
     path('news/', views.NewsListView.as_view(), name='news_list'),
     path('', views.index, name='index'),
+    path('<int:pk>/delete', views.PostDelete.as_view(), name='delete'),
     path('news/<int:id>/', views.news_full_detail, name='news_full_detail'),
-    ##path('<int:id>/', views.NewsDetailView.as_view(), name='news_full_detail'),
+    path('<int:id>/', views.news_full_detail, name='news_full_detail'),
     path('news_search/', views.PostsListView.as_view(), name='news_search'),
     path('news/create/', views.PostCreate.as_view(), name='create'),
     path('news/<int:pk>/edit', views.PostUpdate.as_view(), name='edit'),
