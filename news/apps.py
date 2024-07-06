fec57df6c0a08ecf9240a0cd20a2391722397551
AleKolar <gefest-173@rambler.ru>
2024-07-06 22:13:26 +0300

    commit27.5

--- news/apps.py
+++ news/apps.py
@@ -1,5 +1,4 @@
 from urllib import request
-
 from django.apps import AppConfig
 from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
@@ -43,38 +42,41 @@ class NewsConfig(AppConfig):
             for subscriber in subscribers:
                 user_email = subscriber.email
 
+                list_of_posts = []
+
+                #new_posts = Post.objects.all() # ТЕСТИЛ
                 new_posts = Post.objects.filter(created_at__range=[start_date, end_date],
                                                 post_categories__in=categories)
 
                 email_subject = "Weekly Article/News List"
-                email_body = "List of new articles/news published this week:\n"
-
-                # for post in new_posts:
-                #     post_type = 'articles' if post.post_type == 'article' else 'news'
-                #     current_site = get_current_site(request)
-                #     post_url = f'http://{current_site}{reverse("post_detail", args=[post_type, post.id])}'
-                #     email_body += f"{post.title}: {post.content[:50]} - {get_current_site(None)}{post_url}\n"
+                email_body = f"List of new articles/news published this week:\n"
 
                 for post in new_posts:
-                    post_type = 'articles' if post.post_type == 'article' else 'news'
-                    #current_site = get_current_site(request)
+                    post_type = 'article' if post.post_type == 'article' else 'news'
                     post_url = f'http://127.0.0.1:8000/login/protect/{post.id}'
-                    email_body += f"{post_type} {post.title}: {post.content[:50]} - {get_current_site(None)}{post_url}\n"
-
-                    post.created_at = timezone.make_aware(post.created_at)
+                    post_link = f'<a href="{post_url}">{post.title}</a>'
+                    email_body += f"{post_type}: {post_link} - {post.content[:50]}\n"
+                    list_of_posts.append({'title': post.title, 'content': post.content[:50], 'post_url': post_url})
 
                 subject = 'Weekly Article/News List'
-                message = render_to_string('weekly_email_template.html', {
+                html_message = render_to_string('weekly_email_template.html', {
                     'email_body': email_body,
+                    'new_posts': list_of_posts,
                 })
+                plain_message = "This is the plain text version of the email."
 
-                email = EmailMultiAlternatives(subject, email_body, 'gefest-173@yandex.ru', [user_email])
-                email.attach_alternative(message, 'text/html')
-                email.send()
+                send_mail(
+                    subject,
+                    plain_message,
+                    'gefest-173@yandex.ru',
+                    [user_email],
+                    html_message=html_message,
+                )
 
         scheduler.add_job(send_weekly_article_list, 'cron', day_of_week='mon', hour=8)
         scheduler.start()
 
+        # Тестил, опасная вещь к базе данных нельзя, то нельзя , не успел импортироваться
         # schedule.every(30).seconds.do(send_weekly_article_list)
         #
         # while True:
