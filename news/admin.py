96c15bab9a138d4de2ac881b8af2027cb308c21e
AleKolar <gefest-173@rambler.ru>
2024-08-10 21:10:18 +0300

    commit31.4

--- news/admin.py
+++ news/admin.py
@@ -5,6 +5,7 @@ from .models import Author, Category, Post, PostCategory, Comment
 class PostAdmin(admin.ModelAdmin):
     list_display = ('post_type', 'title', 'content', 'created_at', 'author', 'last_news')
     list_filter = ('created_at', 'post_type')
+    search_fields = 'content', 'title'
 
 
 admin.site.register(Author)
